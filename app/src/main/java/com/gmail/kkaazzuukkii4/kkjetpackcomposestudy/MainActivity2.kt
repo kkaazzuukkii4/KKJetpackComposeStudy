f96b085ccd3a64351e2bc7a622466f34db39d4b3
Kazuki K <kkaazzuukkii4@gmail.com>
2023-10-31 06:18:46 +0900

    chapter8

--- app/src/main/java/com/gmail/kkaazzuukkii4/kkjetpackcomposestudy/MainActivity2.kt
+++ app/src/main/java/com/gmail/kkaazzuukkii4/kkjetpackcomposestudy/MainActivity2.kt
@@ -3,17 +3,22 @@ package com.gmail.kkaazzuukkii4.kkjetpackcomposestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,14 +29,46 @@ class MainActivity2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KKJetpackComposeStudyTheme {
-                MyApp(Modifier.fillMaxSize())
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-private fun MyApp(
+private fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit, // 引数で関数を渡す
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
+fun Greetings(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
@@ -74,10 +111,27 @@ fun Greeting(name: String) {
     }
 }
 
+@Preview
+@Composable
+fun MyAppPreview() {
+    KKJetpackComposeStudyTheme {
+        MyApp()
+    }
+}
+
 @Preview(showBackground = true, widthDp = 320)
 @Composable
-fun GreetingPreview() {
+fun GreetingsPreview() {
     KKJetpackComposeStudyTheme {
-        MyApp(Modifier.fillMaxSize())
+        Greetings()
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun OnboardingPreview() {
+    KKJetpackComposeStudyTheme {
+        OnboardingScreen(onContinueClicked = {}) // 空のラムダ式＝何もしない
     }
 }
