ef81fb77fd6e634be1f55e70c8c5849bf4e43fcb
Kazuki K <kkaazzuukkii4@gmail.com>
2023-10-31 06:56:40 +0900

    chapter12

--- app/src/main/java/com/gmail/kkaazzuukkii4/kkjetpackcomposestudy/MainActivity2.kt
+++ app/src/main/java/com/gmail/kkaazzuukkii4/kkjetpackcomposestudy/MainActivity2.kt
@@ -1,5 +1,6 @@
 package com.gmail.kkaazzuukkii4.kkjetpackcomposestudy
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -26,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gmail.kkaazzuukkii4.kkjetpackcomposestudy.ui.theme.KKJetpackComposeStudyTheme
@@ -116,7 +118,13 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name,
+                    // copyを使い定義済みのスタイルの一部を変更
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(onClick = {
                 expanded.value = !expanded.value
@@ -127,9 +135,14 @@ fun Greeting(name: String) {
     }
 }
 
-@Preview
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Composable
-fun MyAppPreview() {
+fun DefaultPreview() {
     KKJetpackComposeStudyTheme {
         MyApp()
     }
