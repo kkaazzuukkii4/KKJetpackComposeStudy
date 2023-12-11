6bfff573edb1c92099bfcb69f80028e69742848a
Kazuki K <kkaazzuukkii4@gmail.com>
2023-12-12 05:59:28 +0900

    chapter5

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/theme/yamato/Theme.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/theme/yamato/Theme.kt
@@ -16,11 +16,15 @@
 
 package com.example.reply.ui.theme.yamato
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -92,10 +96,14 @@ fun AppTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    val colors = if (!useDarkTheme) {
-        LightColors
-    } else {
-        DarkColors
+    val context = LocalContext.current
+    val colors = when {
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+            if (useDarkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+        useDarkTheme -> DarkColors
+        else -> LightColors
     }
 
     MaterialTheme(
